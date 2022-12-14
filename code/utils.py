e5049e60f0a06bae9beecdb45b9a881ad0504463
Subei Han <hansubei126@gmail.com>
2022-12-14 01:39:02 -0500

    Simplify utils.py

--- code/utils.py
+++ code/utils.py
@@ -16,45 +16,6 @@ import numpy as np
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
-# NOTE: assumes that the epoch starts with 1
-def adjust_learning_rate(epoch, opt, optimizer):
-    if hasattr(opt, 'cos') and opt.cos:
-        # NOTE: since epoch starts with 1, we have to subtract 1
-        new_lr = opt.learning_rate * 0.5 * (1. + math.cos(math.pi * (epoch-1) / opt.epochs))
-        print('LR: {}'.format(new_lr))
-        for param_group in optimizer.param_groups:
-            param_group['lr'] = new_lr
-    else:
-        steps = np.sum(epoch > np.asarray(opt.lr_decay_epochs))
-        if steps > 0:
-            new_lr = opt.learning_rate * (opt.lr_decay_rate ** steps)
-            print('LR: {}'.format(new_lr))
-            for param_group in optimizer.param_groups:
-                param_group['lr'] = new_lr
-
-
-class AverageMeter(object):
-    """Computes and stores the average and current value"""
-    def __init__(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
-
-
 def accuracy(outputs, labels):
     total = 0
     correct = 0
