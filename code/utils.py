648d5c136695e10ded6fc8976fb9df50b3c59691
Subei Han <hansubei126@gmail.com>
2022-12-13 23:51:54 -0500

    Update accuracy function

--- code/utils.py
+++ code/utils.py
@@ -55,21 +55,13 @@ class AverageMeter(object):
         self.avg = self.sum / self.count
 
 
-def accuracy(output, target, topk=(1,)):
-    """Computes the accuracy over the k top predictions for the specified values of k"""
-    with torch.no_grad():
-        maxk = max(topk)
-        batch_size = target.size(0)
-
-        _, pred = output.topk(maxk, 1, True, True)
-        pred = pred.t()
-        correct = pred.eq(target.view(1, -1).expand_as(pred))
-
-        res = []
-        for k in topk:
-            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
-            res.append(correct_k.mul_(100.0 / batch_size))
-        return res
+def accuracy(outputs, labels):
+    total = 0
+    correct = 0
+    _, predicted = torch.max(outputs.data, 1)
+    total += labels.size(0)
+    correct += (predicted == labels).sum().item()
+    return correct/total
 
 class TwoCropsTransform:
     def __init__(self, k_t, q_t):
